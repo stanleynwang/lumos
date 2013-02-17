5a5523106dba97f15f075cf6d5dcdaf7858f27d0
Wilson Lee <kourge@gmail.com>
2013-02-16 16:03:05 -0800

    Add new delegate method

--- Octyrion/HAAppDelegate.m
+++ Octyrion/HAAppDelegate.m
@@ -52,6 +52,11 @@
   //
 }
 
+- (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
+{
+  //
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
